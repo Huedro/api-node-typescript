834ee61575ae3833682560f36a0e13d15b37a659
Pedro Augusto <hpedroaugusto@hotmail.com>
2024-08-29 19:49:24 -0300

    added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import express from 'express';
 
 
 const server = express();
-
 
 server.get('/', (req, res) => {
     return res.send('Olá, DEV!');
