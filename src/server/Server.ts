0e3efca1572106a5d60f5d8e8bae7baf99cf8d07
Pedro Augusto <hpedroaugusto@hotmail.com>
2024-08-31 11:33:04 -0300

    added env and build configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import express from 'express';
+import 'dotenv/config';
 import { router } from './routes';
 
 const server = express();
